9bff55d0b6a35e5e1311e5b3bcfb7f4b7ef6ba77
Mariam Binte Mahfuz <bintemahfuz92@gmail.com>
2022-07-07 21:34:53 -0700

    Some changes

--- script.py
+++ script.py
@@ -3,7 +3,7 @@
 import json
 from googletrans import Translator
 from fpdf import FPDF
-from pytz import unicode
+# from pytz import unicode
 
 translator = Translator()
 #Fetching Json data
@@ -14,25 +14,23 @@ with open('json.json') as f:
     hindi=translator.translate(txt1, dest='hi')
     chinese=translator.translate(txt1, dest='zh-cn')
     japanese=translator.translate(txt1, dest='ja')
+    #encoding function
+    # def lt(str):
+    #     return unicode(str, 'utf-8').encode('iso-8859-1')
 
-    # ubangla = unicode('bangla', 'utf-8')
-    # sbangla = ubangla.encode('iso-8859-1')
-
-
-    def lt(str):
-        return unicode(str, 'utf-8').encode('iso-8859-1')
     #PDF Create and Printing Translation
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=16)
-    # pdf.cell(50,20, sbangla, 0, 2, 'L')
-    pdf.cell(50, 20, lt(bangla), 0, 2, 'L')
-    # pdf.cell(200, 10, txt=str(hindi),
-    #             ln=2, align='C')
-    # pdf.cell(200, 10, txt=str(chinese),
-    #             ln=2, align='C')
-    # pdf.cell(200, 10, txt=str(japanese),
-    #             ln=2, align='C')
+    # pdf.cell(50, 20, lt(bangla), 0, 2, 'L')
+    pdf.cell(200, 10, txt=str(bangla),
+             ln=2, align='C')
+    pdf.cell(200, 10, txt=str(hindi),
+                ln=2, align='C')
+    pdf.cell(200, 10, txt=str(chinese),
+                ln=2, align='C')
+    pdf.cell(200, 10, txt=str(japanese),
+                ln=2, align='C')
     pdf.output("output1.pdf")
     # print(bangla)
     # print(hindi)
